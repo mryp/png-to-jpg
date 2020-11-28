5acb6b1f9c46843579f3466d62ee2a6be7ad7b0b
mryp <picobox.org@gmail.com>
2020-11-28 20:33:40 +0900

    ベースファイル追加

---
+++ PngToJpg/PngToJpg/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PngToJpg
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            Log.Info("アプリケーション起動");
+        }
+
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Log.Info("アプリケーション終了");
+        }
+    }
+}
